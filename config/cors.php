8960a5e1272b912365487c2130de1b1b3d03028b
phandyphorn <phornphandy20@gmail.com>
2026-06-30 15:56:06 +0700

    fix in cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://devremedy-todo.netlify.app/', 'http://localhost:3000'],
+    'allowed_origins' => ['https://devremedy-todo.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
