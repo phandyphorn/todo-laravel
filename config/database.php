da9cdfedf2c861337f15cb820c9db390b5f5fd37
phandyphorn <phornphandy20@gmail.com>
2026-07-01 14:46:14 +0700

    update url in pgsql

--- config/database.php
+++ config/database.php
@@ -86,8 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            // 'url' => env('DB_URL'),
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -150,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
